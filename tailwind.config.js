aa485f97b08d9b067cd22bdd43d56380b81b901d
alissoncesar <alissoncesar1113@gmail.com>
2024-11-14 11:41:41 -0300

    inicio de projeto

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: [],
+    theme: {
+      extend: {
+        colors: {
+            'terracota': '#64221D',  
+            'mostarda': '#F7AE1A',
+            'preto-medio': '#292625',
+            'marron-escuro': '#3B2416',
+            'marron-claro': '#A97744',
+            'nude': '#F5E7DA',
+
+
+          }
+      },
+    },
+    plugins: [],
+  }
